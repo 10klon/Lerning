dcbff167db3dc198aadc9b0e554c5eb7f86722dd
10klon <104237490+10klon@users.noreply.github.com>
2026-02-04 03:18:24 +0300

    Skillbox

---
+++ SkillBox_C++/Lesson/L_5/Bank.cpp
@@ -0,0 +1,151 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <consoleapi2.h>
+#include <cstdlib>
+using namespace std;
+
+int main() {
+	SetConsoleCP(1251);        // кодировка для ввода (cin)
+	SetConsoleOutputCP(1251);  // кодировка для вывода (cout)
+
+	cout << "_____Банкомат_____ \n";
+
+	int cash;
+	int cashTotal;
+	int a = 100;
+	int b = 200;
+	int c = 500;
+	int d = 1000;
+	int f = 2000;
+	int e = 5000;
+
+	cout << "Введите какую сумму хотите получить?  \n";
+	cin >> cash;
+	if (cash == 0) {
+		cout << "Вы запросили нулевую сумму. Операция завершена.\n";
+		return 0;
+	}
+	if (cash > 150000)
+	{
+		cout << " Сумма слишком большая! \n";
+		return 0;
+	}
+	if (cash % 100 == 0)
+	{
+
+		if (cash >= 5000 && cash % 5000 == 0) {
+
+			e = cash / 5000;
+			cout << " Выдаю по 5000: " << e << "\n";
+		}
+		else
+		{
+			e = cash / 5000;
+			cashTotal = cash % 5000;
+			cout << " Выдаю по 5000: " << e << "\n";
+			if (cashTotal >= 2000 && cashTotal % 2000 == 0)
+			{
+				f = cashTotal / 2000;
+				cout << " Выдаю по 2000: " << f << "\n";
+			}
+			else
+			{
+
+				f = cashTotal / 2000;
+				cashTotal = cashTotal % 2000;
+				cout << " Выдаю по 2000: " << f << "\n";
+
+				if (cashTotal >= 1000 && cashTotal % 1000 == 0)
+				{
+					d = cashTotal / 1000;
+					cout << " Выдаю по 1000: " << d << "\n";
+				}
+				else
+				{
+
+					d = cashTotal / 1000;
+					cashTotal = cashTotal % 1000;
+					cout << " Выдаю по 1000: " << d << "\n";
+					if (cashTotal >= 500 && cashTotal % 500 == 0)
+					{
+						c = cashTotal / 500;
+						cout << " Выдаю по 500: " << c << "\n";
+					}
+					else {
+						c = cashTotal / 500;
+						cashTotal = cashTotal % 500;
+						cout << " Выдаю по 500: " << c << "\n";
+						if (cashTotal >= 200 && cashTotal % 200 == 0)
+						{
+							b = cashTotal / 200;
+							cout << " Выдаю по 200: " << b << "\n";
+						}
+						else {
+							b = cashTotal / 200;
+							cashTotal = cashTotal % 200;
+							cout << " Выдаю по 200: " << b << "\n";
+
+							if (cashTotal >= 100 && cashTotal % 100 == 0)
+							{
+								a = cashTotal / 100;
+								cout << " Выдаю по 100: " << a << "\n";
+							}
+							else {
+								a = cashTotal / 100;
+								cashTotal = cashTotal % 100;
+								cout << " Выдаю по 100: " << a << "\n";
+
+							}
+
+						}
+
+					}
+				}
+
+			}
+
+
+		}
+
+	}
+	else {
+		cout << " Невозможно выдать такую сумму \n";
+
+	}
+
+
+
+
+
+
+
+
+
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ SkillBox_C++/Lesson/L_5/Bank2.cpp
@@ -0,0 +1,143 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <consoleapi2.h>
+#include <cstdlib>
+using namespace std;
+
+int main() {
+	SetConsoleCP(1251);        // кодировка для ввода (cin)
+	SetConsoleOutputCP(1251);  // кодировка для вывода (cout)
+
+	cout << "_____Банкомат_____ \n";
+
+	int cash;
+
+	int a = 100;
+	int b = 200;
+	int c = 500;
+	int d = 1000;
+	int f = 2000;
+	int e = 5000;
+
+	cout << "Введите какую сумму хотите получить?  \n";
+	cin >> cash;
+
+	if (cash == 0) {
+		cout << "Вы запросили нулевую сумму. Операция завершена.\n";
+		return 0;
+	}
+	if (cash > 150000)
+	{
+		cout << " Сумма слишком большая! \n";
+		return 0;
+	}
+	if (cash % 100 == 0)
+	{
+		e = cash / 5000;
+		if (e != 0) {
+
+
+
+			cout << " Выдаю по 5000: " << e << "\n";
+
+		}
+		int cash_2000 = cash % 5000;
+		f = cash_2000 / 2000;
+		if (f != 0)
+		{
+
+
+			cout << " Выдаю по 2000: " << f << "\n";
+
+		}
+		int cash_1000 = cash % 5000;
+		cash_1000 = cash_1000 % 2000;
+		d = cash_1000 / 1000;
+		if (d != 0)
+		{
+
+
+			cout << " Выдаю по 1000: " << d << "\n";
+		}
+		int cash_500 = cash % 1000;
+		c = cash_500 / 500;
+
+		if (c != 0)
+		{
+
+
+			cout << " Выдаю по 500: " << c << "\n";
+		}
+		int cash_200 = cash % 500;
+		b = cash_200 / 200;
+		if (b != 0)
+		{
+
+
+			cout << " Выдаю по 200: " << b << "\n";
+
+		}
+		int cash_100 = cash % 500;
+		cash_100 = cash_100 % 200;
+		a = cash_100 / 100;
+
+		if (a != 0)
+		{
+
+
+			cout << " Выдаю по 100: " << a << "\n";
+		}
+
+	}
+	else {
+		cout << " Невозможно выдать такую сумму \n";
+
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
