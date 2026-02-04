c700971d48f0552d8773a1bf34ae664d6a79f820
10klon <104237490+10klon@users.noreply.github.com>
2026-02-04 18:04:42 +0300

    SkillBox

---
+++ SkillBox_C++/Lesson/L_5/Birthday.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <consoleapi2.h>
+#include <cstdlib>
+using namespace std;
+
+int main() {
+    SetConsoleCP(1251);        // кодировка для ввода (cin)
+    SetConsoleOutputCP(1251);  // кодировка для вывода (cout)
+    int todayDay, todayMonth, todayYear;
+    int birthDay, birthMonth, birthYear;
+    int age;
+
+
+    
+    cout << "Введите сегодняшнюю дату (день месяц год): ";
+    cin >> todayDay >> todayMonth >> todayYear;
+
+    
+    cout << "Введите дату рождения покупателя (день месяц год): ";
+    cin >> birthDay >> birthMonth >> birthYear;
+
+    
+    age = todayYear - birthYear;
+    if (age > 18)
+    {
+        cout << "можно" << endl;
+        return 0;
+
+    }
+
+
+    
+    bool birthdayPassed = (todayMonth > birthMonth) ||
+        (todayMonth == birthMonth && todayDay > birthDay);
+
+
+    
+    if (birthdayPassed) {
+        cout << "можно" << endl;
+    }
+    else {
+        cout << "нельзя" << endl;
+    }
+
+    return 0;
+}
---
+++ SkillBox_C++/Lesson/L_5/Boxes.cpp
@@ -0,0 +1,95 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <consoleapi2.h>
+#include <cstdlib>
+using namespace std;
+
+int main() {
+	SetConsoleCP(1251);        // кодировка для ввода (cin)
+	SetConsoleOutputCP(1251);  // кодировка для вывода (cout)
+	int a, b, c, m, n, k, z, z2;
+
+	while (true)
+	{
+
+
+		cout << "_____Boxes_____ \n";
+		cout << " Введите Ш.В.Г первой коробки\n";
+
+		if (!(cin >> a >> b >> c))
+		{
+			cout << " Error \n";
+			return 1;
+		}
+		if (a <= 0 || b <= 0 || c <= 0)
+		{
+			cout << " Error \n";
+			return 1;
+		}
+		cout << " Введите Ш.В.Г второй коробки \n";
+
+		if (!(cin >> m >> n >> k))
+		{
+			cout << " Error \n";
+			return 1;
+		}
+		if (m <= 0 || n <= 0 || k <= 0)
+		{
+			cout << " Error \n";
+			return 1;
+		}
+
+		cout << " Проверяем поместится ли. \n";
+
+		if (a * b * c < m * n * k)
+
+		{
+			cout << " Не поместится объем превышен \n";
+		}
+		if (a >= m && b >= n && c >= k || a >= m && b >= k && c >= n || a >= k && b >= m && c >= n || a >= k && b >= n && c >= m || a >= n && b >= k && c >= m || a >= n && b >= m && c >= k) {
+
+			cout << " Поместится \n";
+
+		}
+
+
+
+		else
+		{
+			cout << " Не поместится \n";
+		}
+
+
+
+
+	}
+
+
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
